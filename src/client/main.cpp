b18bdb7af906b07224907c476421b18fb1a968f7
laurentayme <vincent.ayme@ensea.fr>
2018-09-26 08:27:19 +0200

    1.1

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <stdio.h>
+#include <string.h>
 
 // Les lignes suivantes ne servent qu'à vérifier que la compilation avec SFML fonctionne
 #include <SFML/Graphics.hpp>
@@ -19,7 +21,14 @@ int main(int argc,char* argv[])
     Exemple exemple;
     exemple.setX(53);
 
-    cout << "It works !" << endl;
-
+    /*cout << "It works !" << endl;*/
+    if (argc>1){                 // vérifie s'il y a un argument
+        if  (strcmp(argv[1],"hello")==0) {   // vérification que l'argument est le bon
+            cout << "Bonjour le monde!" << endl;
+        
+        
+        }
+    }
+    
     return 0;
 }
