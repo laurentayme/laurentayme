3dfb2aacb3e1536431924428a0d16aaa4730a77f
laurentayme <vincent.ayme@ensea.fr>
2018-10-22 11:52:42 +0200

    main avec map de tiles

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -91,12 +91,91 @@ void testSFML() {
 
 // Fin test SFML
 
+// test map
+
+class TileMap : public sf::Drawable, public sf::Transformable
+{
+public:
+
+    bool load(const std::string& tileset, sf::Vector2u tileSize, const int* tiles, unsigned int width, unsigned int height)
+    {
+        // on charge la texture du tileset
+        if (!m_tileset.loadFromFile(tileset))
+            return false;
+
+        // on redimensionne le tableau de vertex pour qu'il puisse contenir tout le niveau
+        m_vertices.setPrimitiveType(sf::Quads);
+        m_vertices.resize(2*width * height * 4);
+
+
+        // on remplit le tableau de vertex, avec un quad par tuile
+        for (unsigned int i = 0; i < width*2; ++i)
+            for (unsigned int j = 0; j < height; ++j)
+            {
+                // on récupère le numéro de tuile courant
+                int tileNumber = tiles[i + j * width];
+
+                // on en déduit sa position dans la texture du tileset
+                int tu = tileNumber % (m_tileset.getSize().x / tileSize.x);
+                int tv = tileNumber / (m_tileset.getSize().x / tileSize.x);
+
+                // on récupère un pointeur vers le quad à définir dans le tableau de vertex
+                sf::Vertex* quad = &m_vertices[(i + j * width*2) * 4];
+		
+
+                // on définit ses quatre coins
+		if(i%2==0){
+                	quad[0].position = sf::Vector2f(i/2 * tileSize.x+32, j * tileSize.y);
+                	quad[1].position = sf::Vector2f((i/2 + 1) * tileSize.x, j * tileSize.y+16);
+                	quad[2].position = sf::Vector2f((i/2 + 1) * tileSize.x-32, (j + 1) * tileSize.y);
+                	quad[3].position = sf::Vector2f(i/2 * tileSize.x, (j + 1) * tileSize.y-16);
+		}
+		else{
+			quad[0].position = sf::Vector2f((i+1)/2 * tileSize.x, j * tileSize.y+16);
+                	quad[1].position = sf::Vector2f(((i+1)/2 + 1) * tileSize.x-32, (j+1) * tileSize.y);
+                	quad[2].position = sf::Vector2f((i+1)/2 * tileSize.x, (j + 1) * tileSize.y+16);
+                	quad[3].position = sf::Vector2f((i+1)/2 * tileSize.x-32, (j + 1) * tileSize.y);
+		}
+                // on définit ses quatre coordonnées de texture
+                quad[0].texCoords = sf::Vector2f(tu * tileSize.x+32, tv * tileSize.y);
+                quad[1].texCoords = sf::Vector2f((tu + 1) * tileSize.x, tv * tileSize.y+16);
+                quad[2].texCoords = sf::Vector2f((tu + 1) * tileSize.x-32, (tv + 1) * tileSize.y);
+                quad[3].texCoords = sf::Vector2f(tu * tileSize.x, (tv + 1) * tileSize.y-16);
+		
+            }
+
+        return true;
+    }
+
+private:
+
+    virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const
+    {
+        // on applique la transformation
+        states.transform *= getTransform();
+
+        // on applique la texture du tileset
+        states.texture = &m_tileset;
+
+        // et on dessine enfin le tableau de vertex
+        target.draw(m_vertices, states);
+
+    }
+
+    sf::VertexArray m_vertices;
+    sf::Texture m_tileset;
+};
+
+//fin test map
+
+
+
 void Test_Unitaire();
 
 
 int main(int argc,char* argv[])
 {
-    testSFML();
+    //testSFML();
     if (argc>1){                 // vérifie s'il y a un argument
         if  (strcmp(argv[1],"hello")==0) {   // vérification que l'argument est le bon
             cout << "Bonjour le monde!" << endl;
@@ -146,6 +225,50 @@ int main(int argc,char* argv[])
     ElementTab carte(1,2,elementList);
   */
     
+//test map
+	// on crée la fenêtre
+    sf::RenderWindow window(sf::VideoMode(640, 320), "Tilemap");
+
+    // on définit le niveau à l'aide de numéro de tuiles
+    const int level[] =
+    {
+       	0, 0, 0, 0, 0, 0, 1, 1, 1, 1,1, 1, 1, 1, 1, 1,0, 1, 1, 1, 
+	1, 1, 1, 0, 0, 0, 0, 2, 0, 0,0, 0,1, 1, 0, 0, 0, 0, 0, 0,
+	3, 3, 3, 3, 3, 3, 3, 3,0, 1,0, 0, 2, 0, 4, 4, 3, 0, 1, 1,
+	1, 0, 0, 0,0, 1, 1, 0, 3, 3,3, 7, 7, 0, 1, 1, 1, 2, 0, 0,
+	1, 0, 1, 0, 1, 0, 1, 0, 1, 0,1 ,0, 0, 5, 4, 6, 8, 8, 8, 7,
+	0, 0, 0, 0, 0, 0, 1, 1, 1, 1,1, 1, 1, 1, 1, 1,0, 1, 1, 1, 
+	1, 1, 1, 0, 0, 0, 0, 2, 0, 0,0, 0,1, 1, 0, 0, 0, 0, 0, 0,
+	3, 3, 3, 3, 3, 3, 3, 3,0, 1,0, 0, 2, 0, 4, 4, 3, 0, 1, 1,
+	1, 0, 0, 0,0, 1, 1, 0, 3, 3,3, 7, 7, 0, 1, 1, 1, 2, 0, 0,
+	1, 0, 1, 0, 1, 0, 1, 0, 1, 0,1 ,0, 0, 5, 4, 6, 8, 8, 8, 7
+    };
+
+    // on crée la tilemap avec le niveau précédemment défini
+    TileMap map;
+    if (!map.load("/home/vincent/Bureau/pltimages/tilsetsaisonplat.png", sf::Vector2u(64, 32), level, 10,10))
+        return -1;
+
+    // on fait tourner la boucle principale
+    while (window.isOpen())
+    {
+        // on gère les évènements
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if(event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        // on dessine le niveau
+        window.clear();
+        window.draw(map);
+        window.display();
+    }
+//fin test mapS
+
+
+
     return 0;
 }
 
